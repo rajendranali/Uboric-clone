944bad70d979b4486c21336bb5cda0a66300bf9b
Saraswati121 <99686266+Saraswati121@users.noreply.github.com>
2022-05-04 19:53:14 +0530

    contn

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,13 @@
 import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
+import {Home } from './Component/HomePage/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div className='first'>
-      <img src="https://www.uboric.com/wp-content/uploads/elementor/thumbs/kitchen-ware-heading-pkx8tvxfmbbsdlrzrk3iwlbxruc7cn5sq339xkx9cg.png"/>
-      <h1 >KITCHEN WARE</h1>
-      <img src="https://www.uboric.com/wp-content/uploads/elementor/thumbs/kitchen-ware-heading-pkx8tvxfmbbsdlrzrk3iwlbxruc7cn5sq339xkx9cg.png"/>
-      </div>
-
-      <div className='sec'>
-      <img src="https://www.uboric.com/wp-content/uploads/2020/12/kithchen-tools-768x602.jpg" />
-      <img src="https://www.uboric.com/wp-content/uploads/2020/12/Air-Tight-Containers-768x602.jpg" />
-      <img src="https://www.uboric.com/wp-content/uploads/2020/12/Gas-Lighter-1024x1024.jpg" />
-    </div>
-
-    <div className='thrd'>
-      <img src="https://www.uboric.com/wp-content/uploads/2020/08/Uboric-1.jpg" alt="" />
-    </div>
-
-    <div className='first'>
-      <img src="https://www.uboric.com/wp-content/uploads/elementor/thumbs/other-category-heading-pkx8tu1r8n97qduq2ja9rlt0l2lgx8yc1tsaz101ow.png" alt="" />
-      <h1> OTHER <br/> CATEGORIES</h1>
-      <img src="https://www.uboric.com/wp-content/uploads/elementor/thumbs/other-category-heading-pkx8tu1r8n97qduq2ja9rlt0l2lgx8yc1tsaz101ow.png" alt="" />
-    </div>
-
-    <div className='same'>
-      <img src="https://www.uboric.com/wp-content/uploads/2021/12/banner1-1-1.jpg" alt="" />
-      <img src="https://www.uboric.com/wp-content/uploads/2021/12/banner2-1.jpg" alt="" />
-    </div>
-
-    <div style={{
-      marginTop:"25px"
-    }}>
-      <img src="https://www.uboric.com/wp-content/uploads/2021/02/Headphone.jpg" alt="" />
-    </div>
-
-    <div className='same'>
-      <img src="https://www.uboric.com/wp-content/uploads/2021/12/banner3-1.jpg" alt="" />
-      <img src="https://www.uboric.com/wp-content/uploads/2021/12/banner4-1.jpg" alt="" />
-    </div>
-
-    <div className='fourth'>
-      <img src="https://www.uboric.com/wp-content/uploads/2020/12/Socks-400x601.jpg" alt="" />
-      <img src="https://www.uboric.com/wp-content/uploads/2020/12/Personal-Care-400x601.jpg" alt="" />
-      <img src="https://www.uboric.com/wp-content/uploads/2020/12/Braclate-400x601.jpg" alt="" />
-      <img src="https://www.uboric.com/wp-content/uploads/2020/12/Grocery-400x601.jpg" alt="" />
-      <img src="https://www.uboric.com/wp-content/uploads/2020/12/Health-Care-400x601.jpg" alt="" />
-    </div>
+      <Home/>
     </div>
   )
 }
